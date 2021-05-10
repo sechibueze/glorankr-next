0cefde04ba5abc9ba45052c8558ab0aced0483db
Samuel Chibueze <sechibueze@gmail.com>
2021-05-10 21:10:46 +0100

    added themeing to app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,8 +4,30 @@ import Brightness6Icon from "@material-ui/icons/Brightness6";
 // import { Brightness6, DonutSmallOutlined } from "@material-ui/icons";
 import Link from "next/link";
 import "../styles/globals.css";
-
+import { useState, useEffect } from "react";
+const THEME = "theme";
 function MyApp({ Component, pageProps }) {
+  const [theme, setTheme] = useState("light");
+  useEffect(() => {
+    const existingTheme = localStorage.getItem(THEME);
+    if (!existingTheme) {
+      saveTheme(theme);
+    } else {
+      saveTheme(existingTheme);
+    }
+  }, []);
+  function saveTheme(theme) {
+    setTheme(theme);
+    localStorage.setItem(THEME, theme);
+    document.documentElement.setAttribute("data-theme", theme);
+  }
+  const changeTheme = () => {
+    if (theme === "light") {
+      saveTheme("dark");
+    } else {
+      saveTheme("light");
+    }
+  };
   return (
     <div className="page-container">
       <nav className="navbar">
@@ -21,7 +43,7 @@ function MyApp({ Component, pageProps }) {
             <h1 className="logo-text">Glorankr </h1>
           </a>
         </Link>
-        <span className="theme-switcher">
+        <span className="theme-switcher" onClick={() => changeTheme()}>
           <Brightness6Icon
             // className="theme-switcher"
             color="primary"
